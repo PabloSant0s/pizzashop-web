9185d3e6b2d6abb405a7b3c7fe7cc78033753d8c
Pablo Santos <pablorasantos.dev@gmail.com>
2024-08-16 11:48:16 -0300

    feat: receita no periodo

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,25 @@
+import { api } from '@/lib/axios'
+
+interface GetDailyRevenueInPeriodParams {
+  from?: Date
+  to?: Date
+}
+
+type GetDailyRevenueInPeriodResponse = {
+  date: string
+  receipt: number
+}[]
+
+export async function getDailyRevenueInPeriod({
+  to,
+  from,
+}: GetDailyRevenueInPeriodParams = {}) {
+  const response = await api.get<GetDailyRevenueInPeriodResponse>(
+    '/metrics/daily-receipt-in-period',
+    {
+      params: { from, to },
+    },
+  )
+
+  return response.data
+}
--- src/pages/app/dashboard/components/revenue-chart.tsx
+++ src/pages/app/dashboard/components/revenue-chart.tsx
@@ -1,6 +1,11 @@
+import { useQuery } from '@tanstack/react-query'
+import { subDays } from 'date-fns'
+import { useMemo, useState } from 'react'
+import { DateRange } from 'react-day-picker'
 import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period'
 import {
   Card,
   CardContent,
@@ -8,17 +13,29 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { DateRangePicker } from '@/components/ui/date-range-picker'
+import { Label } from '@/components/ui/label'
 
-const data = [
-  { date: '10/12', revenue: 1200 },
-  { date: '11/12', revenue: 800 },
-  { date: '12/12', revenue: 900 },
-  { date: '13/12', revenue: 400 },
-  { date: '14/12', revenue: 2300 },
-  { date: '15/12', revenue: 800 },
-  { date: '16/12', revenue: 640 },
-]
 export function RevenueChat() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  })
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ['metrics', 'daily-receipt-in-period', dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({ from: dateRange?.from, to: dateRange?.to }),
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      }
+    })
+  }, [dailyRevenueInPeriod])
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -28,10 +45,18 @@ export function RevenueChat() {
           </CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
         </div>
+        <div className="flex items-center gap-3">
+          <Label htmlFor="periodo">Período</Label>
+          <DateRangePicker
+            id="periodo"
+            date={dateRange}
+            onDateChance={setDateRange}
+          />
+        </div>
       </CardHeader>
       <CardContent>
         <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
+          <LineChart data={chartData} style={{ fontSize: 12 }}>
             <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
             <YAxis
               stroke="#888"
@@ -47,7 +72,7 @@ export function RevenueChat() {
             />
             <Line
               type="linear"
-              dataKey="revenue"
+              dataKey="receipt"
               stroke={colors.violet['500']}
               strokeWidth={2}
             />
