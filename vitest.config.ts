55339ddbced13b7ec933caab7125145cb430befc
Pablo Santos <pablorasantos.dev@gmail.com>
2024-08-22 21:02:43 -0300

    feat: teste unitario sign-in

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
     environment: 'happy-dom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
